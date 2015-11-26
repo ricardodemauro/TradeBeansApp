c9ac8ec72b61b55112ff87123c8ea5a76b239d8a
Ricardo Mauro <ricardo.demauro@gmail.com>
2015-11-26 20:14:00 -0200

    inclusao produto

---
+++ TreadBeans.Models/Product.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreadBeans.Models
+{
+    public class Product : ModelBase
+    {
+        private int id;
+
+        [JsonProperty("id")]
+        public int Id
+        {
+            get { return id; }
+            set { Set(ref id, value); }
+        }
+
+        private string name;
+
+        [JsonProperty("name")]
+        public string Name
+        {
+            get { return name; }
+            set { Set(ref name, value); }
+        }
+
+        private string description;
+
+        [JsonProperty("description")]
+        public string Description
+        {
+            get { return description; }
+            set { Set(ref description, value); }
+        }
+
+        private string brand;
+
+        [JsonProperty("brand")]
+        public string Brand
+        {
+            get { return brand; }
+            set { Set(ref brand, value); }
+        }
+
+    }
+}
